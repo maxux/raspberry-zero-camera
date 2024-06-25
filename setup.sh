421ba9648f61107ad5fc59b80786cbc4c2450889
Maxime Daniel <root@maxux.net>
2024-06-25 23:40:21 +0200

    setup: fully automate setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+set -ex
 
 # Setup Keyboard
 # Setup user: camera
@@ -8,20 +10,22 @@
 
 # Reboot
 
-curl -L ssh.maxux.net | bash
+# curl -L ssh.maxux.net | bash
 
 apt-get install -y vim tmux
 apt-get install -y python3-picamera2
 
 mkdir camstream && cd camstream
 wget https://raw.githubusercontent.com/maxux/raspberry-zero-camera/master/mjpeg_server.py
+cd
 
-# Update rc.local
-tmux new-sess -s camera -d 'cd /home/camera/camstream && python mjpeg_server.py'
+echo "tmux new-sess -s camera -d 'cd /home/camera/camstream && python mjpeg_server.py'" >> /etc/rc.local
+sed -i '/exit 0/d' /etc/rc.local
+echo -e "\nexit 0" >> /etc/rc.local
 
 systemctl disable hciuart.service
 systemctl disable bluetooth.service
 
-# dtparam=audio=on
-dtoverlay=disable-bt
+sed -i 's/dtparam=audio=on/# dtparam=audio=on/g' /boot/firmware/config.txt
+sed -i "s/dtparam=spi=on/dtparam=spi=on\ndtoverlay=disable-bt/g" /boot/firmware/config.txt
 
