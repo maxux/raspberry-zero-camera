e019b73c6d93e3cd3be9cf46d227b5345a599ad6
Maxime Daniel <root@maxux.net>
2024-07-15 01:49:14 +0200

    setup: add rtinfo

--- setup.sh
+++ setup.sh
@@ -13,21 +13,37 @@ set -ex
 # curl -L ssh.maxux.net | bash
 # wget https://raw.githubusercontent.com/maxux/raspberry-zero-camera/master/setup.sh
 
+apt-get update
 apt-get install -y vim tmux
 apt-get install -y python3-picamera2
 
 cd /home/camera
 mkdir camstream && cd camstream
 wget https://raw.githubusercontent.com/maxux/raspberry-zero-camera/master/mjpeg_server.py
-cd
+
+cd /home/camera
+apt-get install -y git
+git clone https://github.com/maxux/librtinfo
+git clone https://github.com/maxux/rtinfo
+
+cd /home/camera/librtinfo/linux
+make && make install && ldconfig
+
+cd /home/camera/rtinfo/rtinfo-client
+make
 
 echo "tmux new-sess -s camera -d 'cd /home/camera/camstream && python mjpeg_server.py'" >> /etc/rc.local
+echo "" >> /etc/rc.local
+echo "/home/camera/rtinfo/rtinfo-client/rtinfo-client --host 10.244.0.253 --daemon" >> /etc/rc.local
+echo "" >> /etc/rc.local
+
 sed -i '/exit 0/d' /etc/rc.local
-echo -e "\nexit 0" >> /etc/rc.local
+echo "exit 0" >> /etc/rc.local
 
 systemctl disable hciuart.service
 systemctl disable bluetooth.service
 
 sed -i 's/dtparam=audio=on/# dtparam=audio=on/g' /boot/firmware/config.txt
 sed -i "s/dtparam=spi=on/dtparam=spi=on\n\ndtoverlay=disable-bt/g" /boot/firmware/config.txt
+
 
